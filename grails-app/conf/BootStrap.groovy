f3017fb5d76c1cea1448211fb09cdb5398206909
light <vlight88@gmail.com>
2014-06-10 20:20:02 +0300

    insert start data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,33 +1,53 @@
 import com.fitupness.domain.Role
-import com.fitupness.domain.User
-import com.fitupness.domain.UserRole
 
 class BootStrap {
 
+    def profileTypeService
+    def programStatusService
+
     def init = { servletContext ->
-        def roleUser = new Role(authority: "ROLE_USER")
+
+//        def psRunning = new ProgramStatus(status: 'running')
+//        psRunning.save()
+//        def psDone = new ProgramStatus(status: 'done')
+//        psDone.save()
+//        def psStop = new ProgramStatus(status: 'stop')
+//        psStop.save()
+//        def psSuspend = new ProgramStatus(status: 'suspend')
+//        psSuspend.save()
+
+        programStatusService.init()
+        profileTypeService.init()
+//        def sportsmanType = new ProfileType(type: 'sportsman')
+//        sportsmanType.save(flush: true)
+//        def trainerType = new ProfileType(type: 'trainer')
+//        trainerType.save(flush: true)
+
+        def roleUser = new Role(authority: "ROLE_SPORTSMAN")
         roleUser.save()
         def roleAdmin = new Role(authority: "ROLE_ADMIN")
         roleAdmin.save()
         def roleTrainer = new Role(authority: "ROLE_TRAINER")
         roleTrainer.save()
-        def roleSimple = new Role(authority: "ROLE_SIMPLE")
-        roleSimple.save()
 
-
-        def user = new User(username: 'user@gmail.com', password: '123456')
-        user.save()
-        def admin = new User(username: 'admin@gmail.com', password: '123456')
-        admin.save()
-        def trainer = new User(username: 'trainer@gmail.com', password: '123456')
-        trainer.save()
-        def simple = new User(username: 'simple@gmail.com', password: '123456')
-        simple.save()
-
-        UserRole.create(user, roleUser, true)
-        UserRole.create(admin, roleAdmin, true)
-        UserRole.create(trainer, roleTrainer, true)
-        UserRole.create(simple, roleUser)
+//        def user = new User(username: 'user@gmail.com', password: '123456',
+//                profile: new Profile(firstname : 'user', lastname : 'user_lastnae', phone: '12300000',
+//                profileType: profileTypeService.getSportsmanType()))
+//        user.save(flush: true)
+//
+//        def admin = new User(username: 'admin@gmail.com', password: '123456',
+//                profile: new Profile(firstname : 'admin', lastname : 'admin_lastnae', phone: '11111111111',
+//                profileType: profileTypeService.getSportsmanType()))
+//        admin.save(flush: true)
+//
+//        def trainer = new User(username: 'trainer@gmail.com', password: '123456',
+//                profile: new Profile(firstname : 'trainer', lastname : 'trainer_lastnae', phone: '555555555',
+//                profileType: profileTypeService.getTrainerType()))
+//        trainer.save(flush: true)
+//
+//        UserRole.create(user, roleUser)
+//        UserRole.create(admin, roleAdmin)
+//        UserRole.create(trainer, roleTrainer)
 
     }
     def destroy = {
