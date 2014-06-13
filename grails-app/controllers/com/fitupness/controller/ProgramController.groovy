efaac36fd0b01a89e320c20afd65257ebc3c058c
light <vlight88@gmail.com>
2014-06-13 18:21:57 +0300

    add program controller delete func

--- grails-app/controllers/com/fitupness/controller/ProgramController.groovy
+++ grails-app/controllers/com/fitupness/controller/ProgramController.groovy
@@ -1,7 +1,6 @@
 package com.fitupness.controller
 
 import com.fitupness.domain.SpGroup
-import com.fitupness.domain.SportProgram
 
 class ProgramController {
 
@@ -11,32 +10,22 @@ class ProgramController {
     def sportProgramService
 
     def index() {
-        def programs = SportProgram.where {
-            owner == profileService.profile
-        }
 
         def groups = SpGroup.where {
             profile == profileService.profile
         }
 
-        def count = SportProgram.count()
-
-        [programs: programs, groups: groups, count: count]
+        [programs: profileService.profile.sportPrograms, groups: groups]
     }
 
     def create() {
-        println params.group
-        def sportProgram = new SportProgram(params)
-        if (sportProgram)
-            sportProgram.owner = profileService.getProfile()
+        sportProgramService.addProgram(params)
 
-        def spGroup = SpGroup.get(params.group)
-        println spGroup
-        if (spGroup)
-            sportProgram.addToGroups(spGroup)
+        redirect action: 'index'
+    }
 
-        sportProgram.save(flush: true)
-
+    def delete() {
+        sportProgramService.deleteProgram(params.id)
         redirect action: 'index'
     }
 
@@ -46,6 +35,11 @@ class ProgramController {
 
     def createGroup() {
         sportProgramService.addGroup(params.name)
+        redirect action: 'groups'
+    }
+
+    def deleteGroup() {
+        sportProgramService.deleteGroup(params.id)
         redirect action: 'groups'
     }
 
