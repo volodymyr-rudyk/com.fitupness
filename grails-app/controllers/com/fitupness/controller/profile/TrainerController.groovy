4086e1b74300d3d2bfca6809bc53371e9770d61c
light <vlight88@gmail.com>
2014-07-03 17:01:35 +0300

    trainer controller claim changes

--- grails-app/controllers/com/fitupness/controller/profile/TrainerController.groovy
+++ grails-app/controllers/com/fitupness/controller/profile/TrainerController.groovy
@@ -1,5 +1,6 @@
 package com.fitupness.controller.profile
 
+import com.fitupness.domain.Claim
 import grails.plugin.springsecurity.annotation.Secured
 
 @Secured(['ROLE_TRAINER'])
@@ -7,9 +8,32 @@ class TrainerController {
 
     def profileService
 
+    def claimService
+
+    def afterInterceptor = { model ->
+        model.claimsCount = claimService.count()
+    }
+
     def index() {
         def trainer = profileService.getTrainer()
-        println trainer
         [trainer: trainer]
+    }
+
+    def sportsmanList() {
+        def trainer = profileService.getTrainer()
+        [sportsmans: trainer.sportsmans]
+    }
+
+    def claimsList() {
+        def claims = Claim.findAllWhere(to: profileService.profile);
+        [claims: claims]
+    }
+
+    def acceptClaim() {
+        forward controller: 'claim', action: 'acceptClaim'
+    }
+
+    def refuseClaim() {
+        forward controller: 'claim', action: 'refuseClaim'
     }
 }
