41c4b416b0ac5ccc54fd59580610997019e4904c
light <vlight88@gmail.com>
2014-07-03 17:03:25 +0300

    user controller claim changes

--- grails-app/controllers/com/fitupness/controller/profile/UserController.groovy
+++ grails-app/controllers/com/fitupness/controller/profile/UserController.groovy
@@ -1,20 +1,33 @@
 package com.fitupness.controller.profile
 
+import com.fitupness.domain.Trainer
 import grails.plugin.springsecurity.annotation.Secured
 
 @Secured(['ROLE_SPORTSMAN'])
 class UserController {
 
+    def claimService
+
     def profileService
 
     def index() {
-        def sportsman = profileService.getSportsman()
-        println sportsman
-        [sportsman: sportsman]
+        def sportsman = profileService.sportsman
+        def tr = Trainer.list()
+        [sportsman: sportsman, trainers: tr]
     }
 
-    def index2() {
+    def trainerList() {
+        def claim = claimService.myClaim()
+        def sportsman = profileService.sportsman
 
+        [sportsman: sportsman,
+         trainers : Trainer.list(),
+         claim    : claim]
+    }
+
+    def addClaim() {
+        claimService.create(params.to)
+        redirect action: 'index'
     }
 
 }
