09a1e490d95b1fa0d9a19ec9026c5fa365977e6e
light <vlight88@gmail.com>
2014-07-10 16:46:22 +0300

    program status correct values

--- grails-app/services/com/fitupness/service/ProgramStatusService.groovy
+++ grails-app/services/com/fitupness/service/ProgramStatusService.groovy
@@ -7,18 +7,18 @@ import grails.transaction.Transactional
 class ProgramStatusService {
 
     public static final String RUNNING = 'running'
-    public static final String STOP = 'stop'
+    public static final String STOPPED = 'stopped'
     public static final String DONE = 'done'
-    public static final String SUSPEND = 'SUSPEND'
+    public static final String SUSPENDED = 'suspended'
 
     def init() {
         def psRunning = new SportProgramStatus(status: RUNNING)
         psRunning.save()
         def psDone = new SportProgramStatus(status: DONE)
         psDone.save()
-        def psStop = new SportProgramStatus(status: STOP)
+        def psStop = new SportProgramStatus(status: STOPPED)
         psStop.save()
-        def psSuspend = new SportProgramStatus(status: SUSPEND)
+        def psSuspend = new SportProgramStatus(status: SUSPENDED)
         psSuspend.save()
     }
 }
