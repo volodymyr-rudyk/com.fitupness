e8a44025ff060c473e45c566f85fc1cf4e94083c
light <vlight88@gmail.com>
2014-07-10 16:46:46 +0300

    sportProgram model updated

--- grails-app/domain/com/fitupness/domain/SportProgram.groovy
+++ grails-app/domain/com/fitupness/domain/SportProgram.groovy
@@ -6,14 +6,24 @@ class SportProgram {
     int repetition
     int count
     int weight
+    Date dateCreated
+    Date lastUpdated
+
+    Sportsman runner
+    SportProgramStatus status
 
     SportProgramType sportProgramType
 
-    static hasMany = [groups: SportProgramGroup]
+    SportProgramGroup sportProgramGroup
+
+//    static hasMany = [groups: SportProgramGroup]
 
     static belongsTo = [trainer: Trainer]
 
     static constraints = {
         sportProgramType nullable: true
+        sportProgramGroup nullable: true
+        runner nullable: true
+        status nullable: true
     }
 }
