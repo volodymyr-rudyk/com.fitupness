07d3255afcc1df0391f79596b63a72ca2d075338
light <vlight88@gmail.com>
2014-07-22 21:18:16 +0300

    add test user and trainer profile to db

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,6 +5,8 @@ class BootStrap {
     def profileTypeService
     def programStatusService
 
+    def authService
+
     def init = { servletContext ->
 
 //        def psRunning = new ProgramStatus(status: 'running')
@@ -30,24 +32,34 @@ class BootStrap {
         def roleTrainer = new Role(authority: "ROLE_TRAINER")
         roleTrainer.save()
 
-//        def user = new User(username: 'user@gmail.com', password: '123456',
-//                profile: new Profile(firstname : 'user', lastname : 'user_lastnae', phone: '12300000',
+//        def sportsman = new User(username: 'vova.rudyk@gmail.com', password: '123456',
+//                profile: new Profile(firstname : 'Vova', lastname : 'Rudyk', phone: '0631788565',
 //                profileType: profileTypeService.getSportsmanType()))
-//        user.save(flush: true)
-//
+//        sportsman.save(flush: true)
+
 //        def admin = new User(username: 'admin@gmail.com', password: '123456',
 //                profile: new Profile(firstname : 'admin', lastname : 'admin_lastnae', phone: '11111111111',
 //                profileType: profileTypeService.getSportsmanType()))
 //        admin.save(flush: true)
-//
+
 //        def trainer = new User(username: 'trainer@gmail.com', password: '123456',
 //                profile: new Profile(firstname : 'trainer', lastname : 'trainer_lastnae', phone: '555555555',
 //                profileType: profileTypeService.getTrainerType()))
 //        trainer.save(flush: true)
-//
-//        UserRole.create(user, roleUser)
+
+//        UserRole.create(sportsman, roleUser)
 //        UserRole.create(admin, roleAdmin)
 //        UserRole.create(trainer, roleTrainer)
+
+
+        def p1 = [phone: '00000000', username: 'user@gmail.com', lastname: 'UserLast', firstname: 'UserFirst', type: '1', password: '123456']
+
+        authService.signup(p1)
+
+        def p2 = [phone: '00000000', username: 'trainer@gmail.com', lastname: 'TrainerLast', firstname: 'TrainerFirst', type: '2', password: '123456']
+
+        authService.signup(p2)
+
 
     }
     def destroy = {
